ba887671c5eef8cc1ae34ac8b08cb0ad19a25701
akosthurzo <akos.thurzo@liferay.com>
2014-03-11 15:17:04 +0100

    LPS-46597 Move logic to CMIS because init is necessary in case of other repositories, e.g. documentum

--- portal-impl/src/com/liferay/portal/service/impl/RepositoryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/RepositoryLocalServiceImpl.java
@@ -19,7 +19,6 @@ import com.liferay.portal.NoSuchRepositoryException;
 import com.liferay.portal.kernel.bean.ClassLoaderBeanHandler;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.lar.ExportImportThreadLocal;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.BaseRepository;
@@ -480,9 +479,7 @@ public class RepositoryLocalServiceImpl extends RepositoryLocalServiceBaseImpl {
 
 		setupRepository(repositoryId, repository, baseRepository);
 
-		if (!ExportImportThreadLocal.isImportInProcess()) {
-			baseRepository.initRepository();
-		}
+		baseRepository.initRepository();
 
 		return baseRepository;
 	}
