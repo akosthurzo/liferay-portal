973e750cc05e3afa15c0964e223c4d5e73e0b456
akosthurzo <akos.thurzo@liferay.com>
2014-05-06 14:06:58 +0200

    LPS-46584 Put repositoryEntryIds to PK map so it can be used in DLPortletDataHandler

--- portal-impl/src/com/liferay/portlet/documentlibrary/lar/DLPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/lar/DLPortletDataHandler.java
@@ -326,7 +326,7 @@ public class DLPortletDataHandler extends BasePortletDataHandler {
 
 				Map<Long, Long> folderIds =
 					(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
-						Folder.class);
+						Folder.class + ".folderIdsAndRepositoryEntryIds");
 
 				rootFolderId = MapUtil.getLong(
 					folderIds, rootFolderId, rootFolderId);
