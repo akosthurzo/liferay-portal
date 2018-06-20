0e8d486235af5839c5d0b678634b70f7d8a95d0b
akosthurzo <akos.thurzo@liferay.com>
2018-06-20 11:04:23 +0200

    LPS-82680 Refactor buildURL logic to new API

--- portal-kernel/src/com/liferay/exportimport/kernel/staging/StagingUtil.java
+++ portal-kernel/src/com/liferay/exportimport/kernel/staging/StagingUtil.java
@@ -61,12 +61,23 @@ public class StagingUtil {
 		_staging.addModelToChangesetCollection(model);
 	}
 
+	/**
+	 * @deprecated As of 5.0.0, replaced by {@link
+	 *             StagingURLHelper#buildRemoteURL(ExportImportConfiguration)}
+	 */
+	@Deprecated
 	public static String buildRemoteURL(
 		ExportImportConfiguration exportImportConfiguration) {
 
 		return _staging.buildRemoteURL(exportImportConfiguration);
 	}
 
+	/**
+	 * @deprecated As of 5.0.0, replaced by {@link
+	 *             StagingURLHelper#buildRemoteURL(String, int, String,
+	 *             boolean)}
+	 */
+	@Deprecated
 	public static String buildRemoteURL(
 		String remoteAddress, int remotePort, String remotePathContext,
 		boolean secureConnection) {
@@ -89,6 +100,11 @@ public class StagingUtil {
 			remoteGroupId, privateLayout);
 	}
 
+	/**
+	 * @deprecated As of 5.0.0, replaced by {@link
+	 *             StagingURLHelper#buildRemoteURL(UnicodeProperties)}
+	 */
+	@Deprecated
 	public static String buildRemoteURL(
 		UnicodeProperties typeSettingsProperties) {
 
